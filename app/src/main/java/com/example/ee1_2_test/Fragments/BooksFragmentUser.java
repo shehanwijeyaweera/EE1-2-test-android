ee6e69292b377c6a05be1f531a2c29e74c1506d2
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-11-27 22:32:16 +0530

    All books view and single book implemented in user side fragment "Books".

--- app/src/main/java/com/example/ee1_2_test/Fragments/BooksFragmentUser.java
+++ app/src/main/java/com/example/ee1_2_test/Fragments/BooksFragmentUser.java
@@ -1,21 +1,77 @@
 package com.example.ee1_2_test.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ee1_2_test.API.BookstoreApi;
+import com.example.ee1_2_test.Activity.Userhomepage;
+import com.example.ee1_2_test.Activity.viewAllBooksUser;
+import com.example.ee1_2_test.Adapters.userViewAllBooksAdapter;
+import com.example.ee1_2_test.Model.ApiClient;
+import com.example.ee1_2_test.Model.Book;
 import com.example.ee1_2_test.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BooksFragmentUser extends Fragment {
+
+    private BookstoreApi bookstoreApi;
+    ArrayList<Book> books = new ArrayList<>();
+    private userViewAllBooksAdapter ViewAllBooksAdapter;
+    private RecyclerView books_recyclerview_user;
+    private ProgressBar loadingbookdata;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_all_books,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_view_all_books,container,false);
+
+        books_recyclerview_user = view.findViewById(R.id.books_recyclerview_user);
+        books_recyclerview_user.setLayoutManager(new GridLayoutManager(getContext(),3));
+        loadingbookdata = view.findViewById(R.id.progressBar_user);
+
+        loadingbookdata.setVisibility(View.VISIBLE);
+        getResponse();
+        loadingbookdata.setVisibility(View.INVISIBLE);
+
+        return view;
+    }
+
+    private void getResponse() {
+        bookstoreApi = ApiClient.getClient().create(BookstoreApi.class);
+
+        Call<List<Book>> call = bookstoreApi.getBooks();
+
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                books = new ArrayList<>(response.body());
+                ViewAllBooksAdapter = new userViewAllBooksAdapter(getActivity(), books);
+                books_recyclerview_user.setAdapter(ViewAllBooksAdapter);
+                Toast.makeText(getActivity(), "Success",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Toast.makeText(getActivity(), "Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
